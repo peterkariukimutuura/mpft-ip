1189daa0ff1e3e2f52f62dbb105ed990aa9f95fc
peter kariuki mutuura <kariukipeter30@gmail.com>
2017-10-24 05:28:02 -0700

    Add jquery, Animation on the link tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-	
+	var showmylinks=  $(".showmylinks");
+	var mylinks =$(".mylinks");
+	showmylinks.click(function(){
+		mylinks.fadeToggle("slow","linear");
+	});
 
 });
